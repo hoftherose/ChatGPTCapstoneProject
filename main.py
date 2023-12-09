ed345992f3c7058bb5a07cc99cbf186b0ff81395
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-09 14:28:49 -0600

    List assistants

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from typing import Union
+import os
+import logging
 
+from openai import OpenAI
 from fastapi import FastAPI
+
+client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
 
+@app.get("/list/assistants")
+def get_assistants():
+    assistants = client.beta.assistants.list()
+    return assistants
 
-@app.get("/")
-def read_root():
+@app.post("/assistants")
+def create_assistants():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
