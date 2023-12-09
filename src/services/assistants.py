0d7afbb9a4dce891c0045d7e96da6ceafee18994
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-09 17:57:25 -0600

    Format assistant list

--- src/services/assistants.py
+++ src/services/assistants.py
@@ -1,6 +1,6 @@
-import logging
+import datetime
 
-from src.utils.constants import client
+from src.utils.constants import client, logging
 
 def upload_file(path):
     file = client.files.create(
@@ -12,6 +12,24 @@ def upload_file(path):
 def get_assistant(path):
     assistant = client.beta.assistants.retrieve("asst_QHYOXsAQ96vbf3JpmGcoc28D")
     return assistant
+
+def get_assistant_list():
+    assistants = client.beta.assistants.list().model_dump()["data"]
+    return {
+        "num_assistants": len(assistants),
+        "assistants": [{
+            "id": assistant["id"],
+            "created_at": datetime.datetime.fromtimestamp(int(assistant["created_at"])),
+            "description": assistant["description"],
+            "file_ids": assistant["file_ids"],
+            "instructions": assistant["instructions"],
+            "metadata": assistant["metadata"],
+            "model": assistant["model"],
+            "name": assistant["name"],
+            "object": assistant["object"],
+            "tools": [{"type": tool["type"]} for tool in assistant["tools"]],
+        } for assistant in assistants],
+    }
 
 def create_assistant(path):
     file = upload_file(path)
