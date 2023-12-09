c95f0926371681bd233f8fcc22adbebafd86726c
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-09 16:21:40 -0600

    Route error handling

--- src/routes/assistants.py
+++ src/routes/assistants.py
@@ -1,23 +1,37 @@
 import logging
-
 from fastapi import APIRouter, UploadFile
 
 from src.utils.constants import client
-from src.services.assistants import create_assistant
+from src.services.assistants import *
 
 
 assistant_router = APIRouter()
 
 @assistant_router.get("/list")
-def get_assistants():
-    assistants = client.beta.assistants.list()
-    return assistants
+def list_assistants():
+    try:
+        assistants = client.beta.assistants.list()
+        return assistants
+    except Exception as e:
+        logging.error(f"{str(e)}")
+    return {"message": "Failed"}
+
+@assistant_router.get("/{assistant_id}")
+def get_assistants(assistant_id: str):
+    try:
+        assistant = get_assistant(assistant_id)
+        return assistant
+    except Exception as e:
+        logging.error(f"{str(e)}")
+    return {"message": "Failed"}
 
 @assistant_router.post("/")
 def create_assistants(file: UploadFile):
     try:
         create_assistant(file.filename)
+        return {"message": "Succeeded"}
     except FileNotFoundError as e:
         logging.error(f"File \"{file.filename}\" does not exist")
     except Exception as e:
         logging.error(f"{str(e)}")
+    return {"message": "Failed"}
