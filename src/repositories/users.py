c3f0c72d0347fd6d40e4325530e8abc5a57e62a8
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 10:05:26 -0600

    Basic ORM structure users

---
+++ src/repositories/users.py
@@ -0,0 +1,35 @@
+"""Table ORM definitions"""
+from typing import Union, List
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import select, and_
+
+from src.repositories.db import Base, session
+from src.utils.errors import Errors
+from src.utils import SystemCodes
+
+
+class UserTable(Base):
+    """Shows table definition"""
+
+    __tablename__ = "users"
+
+    user_id = Column("user_id", Integer, primary_key=True)
+    user_name = Column("user_name", String(100))
+    user_not_so_secret = Column("user_not_so_secret", String(100))
+    created_at = Column("created_at", Date)
+    created_by = Column("created_by", Date)
+
+    @classmethod
+    async def select_user(cls, userid: int):
+        """Return user information"""
+        stmt = select(
+            cls.user_id,
+            cls.user_name,
+            cls.user_not_so_secret,
+            cls.created_at,
+            cls.created_by,
+        ).where(
+            cls.user_id == userid,
+        )
+        resp = session.execute(stmt)
+        return resp
