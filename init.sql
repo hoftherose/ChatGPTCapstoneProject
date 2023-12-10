932e353cb4f4ae5b19ee9791c5380b1a60b8bccc
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 10:03:41 -0600

    Fix column types

--- init.sql
+++ init.sql
@@ -10,14 +10,14 @@ CREATE TABLE assistants (
     assistant_id SERIAL PRIMARY KEY,
     assistant_name VARCHAR(100) NOT NULL,
     file_path VARCHAR(100) NOT NULL,
-    model DATE NOT NULL,
+    model VARCHAR(100) NOT NULL,
     created_at DATE NOT NULL,
     created_by DATE NOT NULL
 );
 
 CREATE TABLE threads (
     thread_id SERIAL PRIMARY KEY,
-    thread_name INT,
+    thread_name VARCHAR(100),
     user_id INT NOT NULL,
     assistant_id INT NOT NULL,
     created_at DATE NOT NULL,
