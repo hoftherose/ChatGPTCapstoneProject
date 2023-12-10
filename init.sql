c2ece70814370ecaa8ac21025c9bfee0c4a80bd8
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 11:46:48 -0600

    Fix db insert

--- init.sql
+++ init.sql
@@ -3,5 +3,5 @@ CREATE TABLE assistants (
     assistant_name VARCHAR(100) NOT NULL,
     file_path VARCHAR(100) NOT NULL,
     model VARCHAR(100) NOT NULL,
-    created_at DATE NOT NULL
+    created_at TIMESTAMP DEFAULT NOW()
 );
