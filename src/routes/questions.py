93310c8aebea1b8e67d3aef8a6a80a654c173727
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 16:09:05 -0600

    Questions route

---
+++ src/routes/questions.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+from src.routes.responses.error_response import ErrorResponses
+from src.routes.responses import QuestionCreatedResponse
+from src.services.questions import *
+from src.utils.constants import logging
+
+
+questions_router = APIRouter(
+    prefix="/api/v1/queries",
+    tags=["Queries"],
+    responses=ErrorResponses.all(),
+)
+
+@questions_router.post(
+    "/generate/{assistant_id}",
+    response_model=QuestionCreatedResponse().model(),
+    response_description=QuestionCreatedResponse().get("description"),
+)
+def generate(assistant_id: str):
+    try:
+        assistants = generate_questions(assistant_id)
+        return QuestionCreatedResponse().format(assistants)
+    except Exception as e:
+        logging.error(f"{str(e)}")
+    return {"message": "Failed"}
