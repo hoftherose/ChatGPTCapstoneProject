9d4ff9811ee25191d1a59f8edd2e64daac43daa5
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 10:36:47 -0600

    Adjust for model

--- src/services/users.py
+++ src/services/users.py
@@ -2,7 +2,7 @@ from src.repositories.users import UserTable
 
 def get_user_list():
     users = UserTable.select_all_users()
-    return users
+    return {"users": users}
 
 def create_user(name: str, pswd: str):
     # UserTable.insert_user()
