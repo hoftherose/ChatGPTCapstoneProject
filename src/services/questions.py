867086d10505744f03107913157d175aa5cfc683
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 18:50:15 -0600

    Get questions from openai

---
+++ src/services/questions.py
@@ -0,0 +1,30 @@
+import time
+
+from src.repositories.assistants import AssistantsTable
+from src.utils.constants import client
+
+
+def generate_questions(assistant_id: str, num_questions: int):
+    assistant = AssistantsTable.select_assistant(assistant_id).all()[0]
+    thread_id = assistant[3]
+    
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=f"Can you give me {num_questions} questions to quiz me on the contents of the file? If asked before, try not to repeat if possible.",
+    )
+
+    run = client.beta.threads.runs.create(
+        assistant_id=assistant_id,
+        thread_id=thread_id,
+    )
+
+    while run.status != "completed":
+        time.sleep(0.25)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id,
+        )
+
+    messages = client.beta.threads.messages.list(thread_id=thread_id)
+    return [{ "messages": messages.data[0].content[0].text.value }]
