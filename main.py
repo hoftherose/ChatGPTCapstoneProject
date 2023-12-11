34836861290838b84cb1b6bfb63d93f5e15ed823
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-12-10 18:49:24 -0600

    Add questions to app

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ from src.routes import *
 app = FastAPI()
 
 app.include_router(assistant_router)
+app.include_router(questions_router)
 app.include_router(health_router)
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,2 +1,3 @@
 from .assistants import assistant_router
+from .questions import questions_router
 from .health_check import health_router
